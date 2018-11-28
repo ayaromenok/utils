3da36a8bed89ca2f547a1608b022b33de683b09c
Andrey E. Yaromenok <ayaromenok@gmail.com>
2018-11-28 19:47:20 +0100

    missed file change from above

--- ocvMakeTestBatch/main.cpp
+++ ocvMakeTestBatch/main.cpp
@@ -1,8 +1,43 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QFile fOut;
+    QFile fIn;
+    QString sFileIn;
+    QString sFileOut;
+    if (argc > 1)
+    {
+        sFileIn.append(argv[1]);
+        qDebug() << "file: " << sFileIn;
+        QFileInfo fi(sFileIn);
+        if (fi.exists()){
+            qDebug() << "size" << fi.size();
+            fIn.setFileName(sFileIn);
+            if (!fIn.open(QIODevice::ReadOnly | QIODevice::Text)){
+                qDebug() << "can't open" << sFileIn << "to read";
+                return -1;
+            }
+            sFileOut.append(sFileIn.left(sFileIn.length()-3));
+            sFileOut.append("sh");
+            qDebug() << "output to" << sFileOut;
+            QTextStream in(&fIn);
+            while (!in.atEnd()) {
+                QString line = in.readLine();
+                //process_line(line); //remove point from end of string
+                qDebug() << line.left(line.length()-1);
+            }
+
+        } else {
+            qDebug() << "file" << argv[1] << "missed";
+        }
+    }
+    else {
+        qDebug() << "file with test names required";
+    }
 
     return a.exec();
 }
