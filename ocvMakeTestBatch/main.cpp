1e6137aeb7409f4926fe4d53bff46316f33aa020
Andrey E. Yaromenok <ayaromenok@gmail.com>
2018-11-29 09:34:05 +0100

    test script builder for OpenCV is OK.

--- ocvMakeTestBatch/main.cpp
+++ ocvMakeTestBatch/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QFile fOut;
     QFile fIn;
     QString sFileIn;
-    QString sFileOut;
+    QString sFileOut("run_");
     if (argc > 1)
     {
         sFileIn.append(argv[1]);
@@ -25,11 +25,43 @@ int main(int argc, char *argv[])
             sFileOut.append("sh");
             qDebug() << "output to" << sFileOut;
             QTextStream in(&fIn);
-            while (!in.atEnd()) {
-                QString line = in.readLine();
-                //process_line(line); //remove point from end of string
-                qDebug() << line.left(line.length()-1);
+            QString sTestName("opencv_");
+            sTestName.append(sFileIn.left(sFileIn.length()-4));
+            qDebug() << "test name" << sTestName;
+            sFileOut.append("sTestName");
+            sFileOut.append(".sh");
+            fOut.setFileName(sFileOut);
+            if (!fOut.open((QIODevice::WriteOnly|QIODevice::Text))){
+                qDebug() << "can't open" << sFileOut << "to write";
+                return -1;
+            } else {
+                qDebug() << "write to" << sFileOut;
             }
+            QTextStream out(&fOut);
+            while (!in.atEnd()) {
+                QString sLine = in.readLine();
+                QString sLineOut("echo \"");
+//                echo "OCL_DilateFixture_Dilate"
+//                time ./opencv_perf_imgproc --gtest_filter=*OCL_DilateFixture_Dilate* > perf_imgproc_OCL_DilateFixture_Dilate.txt
+                //process_line(line); //remove point from end of string
+                sLine = sLine.left(sLine.length()-1);
+                qDebug() << sLine;
+                sLineOut.append(sLine);
+                sLineOut.append("\"\ntime ./");
+                sLineOut.append(sTestName);
+                sLineOut.append(" --gtest_filter=*");
+                sLineOut.append(sLine);
+                sLineOut.append("* > ");
+                sLineOut.append(sTestName);
+                sLineOut.append("_");
+                sLineOut.append(sLine);
+                sLineOut.append(".txt\n");
+                qDebug() << sLineOut;
+                out << sLineOut ;
+            }
+            fIn.close();
+            fOut.flush();
+            fOut.close();
 
         } else {
             qDebug() << "file" << argv[1] << "missed";
